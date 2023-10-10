2075c4f8f4b55295af5f506f8aba8220f7511ad1
Andrew Ritz <drew@august-harper.com>
2023-10-09 21:52:57 -0400

    - Some starting work on a vue app to test the feature set.

--- TimeMan/timeman-ui/src/App.vue
+++ TimeMan/timeman-ui/src/App.vue
@@ -2,7 +2,7 @@
   <SiteHeader v-bind:loginState="loginState" />
   <router-view />
   <button v-on:click="toggleLogin()">Toggle Login</button>
-  <p>{{this.$dtAuth.IsLoggedIn}}</p>
+  <p>{{ this.$dtAuth.IsLoggedIn }}</p>
 </template>
 
 <script lang="ts">
@@ -17,42 +17,39 @@ import { Options, Vue } from "vue-class-component";
 
   data() {
     return {
-      loginState: this.$dtAuth
-        // isLoggedIn: false,
-        // loginToken: null,   // Special token to help with session tracking.
-        // userID: "drew",
-      
+      loginState: this.$dtAuth,
+      // isLoggedIn: false,
+      // loginToken: null,   // Special token to help with session tracking.
+      // userID: "drew",
     };
   },
   methods: {
     toggleLogin: function () {
-        this.$dtAuth.IsLoggedIn = !this.$dtAuth.IsLoggedIn;
+      // Works great, makes sense to me.
+      //this.$dtAuth.IsLoggedIn = !this.$dtAuth.IsLoggedIn;
 
-      // let loggedIn = this.$dtAuth.IsLoggedIn;
-      // if (!loggedIn)
-      // {
-      //   this.$dtAuth.Login();
-      // }
-      // else
-      // {
-      //   this.$dtAuth.Logout();
-      // }
+      // This updates the state of the class just fine, but the UI doesn't get updated.
+      // What to do?
+      if (!this.$dtAuth.IsLoggedIn) {
+        this.$dtAuth.Login();
+      } else {
+        this.$dtAuth.Logout();
+      }
 
-},
+      // This seems like a hack....
+      //this.$forceUpdate();
+    },
 
     // Log out any connected user, and redirect them to the homepage.
-    logout: function (forcedOut:boolean) {
+    logout: function (forcedOut: boolean) {
       if (this.loginState) {
         this.loginState.isLoggedIn = false;
 
-        if (forcedOut)
-        {
+        if (forcedOut) {
           let redirectTo = this.$router.currentRoute.value.path;
           console.log("We will redirect to: " + redirectTo + " on login....");
           this.$router.push("/login?to=" + redirectTo);
-        }
-        else
-        {
+        } else {
           this.$router.push("/");
         }
       }
@@ -66,9 +63,7 @@ import { Options, Vue } from "vue-class-component";
   },
 })
 export default class App extends Vue {
-
-  
-//  private _value : string;
+  //  private _value : string;
   // public get IsLoggedIn() : boolean {
   //   return false;
   //   //return this._value;
@@ -76,7 +71,6 @@ export default class App extends Vue {
   // public set value(v : string) {
   //   this._value = v;
   // }
-  
 }
 </script>
 
--- TimeMan/timeman-ui/src/plugins/dtAuth.ts
+++ TimeMan/timeman-ui/src/plugins/dtAuth.ts
@@ -4,9 +4,9 @@ import { App, reactive } from 'vue';
 //   IsLoggedIn: false;
 // }
 
-const thingy = {
-  IsLoggedIn: true
-}
+// const thingy = {
+//   IsLoggedIn: true
+// }
 
 class dtAuthHandler {
   private _IsLoggedIn: boolean = false;
@@ -16,14 +16,17 @@ class dtAuthHandler {
   public set IsLoggedIn(value:boolean)
   {
     this._IsLoggedIn = value;
+  //  alert(value);
   }
 
   Login = () => {
-    // NOTE: This is where we would want to bounce against the auth server I think...
-    thingy.IsLoggedIn = true;
+    // More stuff here, internal state, etc.
+   // alert('login');
+    this.IsLoggedIn = true;
   }
 
   Logout = () => {
+    // More stuff here, internal state, etc.
     this.IsLoggedIn = false;
   }
 
@@ -32,7 +35,6 @@ class dtAuthHandler {
 export default class dtAuth {
 
   private authState = new dtAuthHandler();
-
   install = (app: App) => {
     app.config.globalProperties.$dtAuth = reactive(this.authState);
   }
