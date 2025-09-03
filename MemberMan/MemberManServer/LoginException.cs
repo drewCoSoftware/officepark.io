1d7b5c523c8b8815b6db8aa69a74d9d6c9e9cf81
Andrew Ritz <aritz@rugsdirect.com>
2025-09-02 23:30:39 -0400

    - Added a new exception type for failed logins.

---
+++ MemberMan/MemberManServer/LoginException.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+
+namespace MemberManServer;
+
+/// <summary>
+/// Indicates that a login failed.
+/// </summary>
+public class LoginException : Exception
+{
+    public LoginException() { }
+    public LoginException(string message) : base(message) { }
+    public LoginException(string message, Exception inner) : base(message, inner) { }
+    protected LoginException(SerializationInfo info, StreamingContext context)
+        : base(info, context) { }
+}
